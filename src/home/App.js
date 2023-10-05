ffbf49747c0c6e1f4f7a4b2ceb706d7839ae8660
unknown <alfonsoloph@gmail.com>
2023-10-04 18:10:17 -0600

    update profile image before change layout

--- src/home/App.js
+++ src/home/App.js
@@ -1,40 +1,29 @@
-import React, {useState, useEffect, useRef} from "react"
 import '../index.css';
 
-
-import TopMenu from './TopMenu';
-import ImgCarousel from "./ImgCarousel";
-import Summary from './Summary';
-import PlatformsIcons from './PlatformsIcons';
-import ExperienceList from './ExperienceList';
-import Contact from "./Contact";
-
 export const App = () => {
-    const homeRef = useRef();
-    const platRef = useRef();
-    const expRef = useRef();
-    const contRef = useRef();
-    const [width, setWidth] = useState(window.innerWidth);
-    function handleWindowSizeChange() {
-        setWidth(window.innerWidth);
-    }
-    useEffect(() => {
-        window.addEventListener('resize', handleWindowSizeChange);
-        return () => {
-            window.removeEventListener('resize', handleWindowSizeChange);
-        }
-    }, []);
-    const isMobile = width <= 1000;
-    console.log(isMobile);
     return (
-        <div>
-            <TopMenu ref={homeRef} isMobile={isMobile} 
-                dataRef={[homeRef,platRef, expRef, contRef]}/>
-            <ImgCarousel isMobile={isMobile}/>
-            <Summary isMobile={isMobile}/>
-            <PlatformsIcons ref={platRef}/>
-            <ExperienceList  ref={expRef} isMobile={isMobile}/>
-            <Contact ref={contRef} isMobile={isMobile}/>
-        </div>
+        <>
+            <header id='headcontent'>
+                <img src='https://alf0ns0-l0pez.github.io/Portfolio/img/al_logo.png'
+                    alt="alfonsologo"  />
+                <section>
+                    <img className='pcbimg' src='https://alf0ns0-l0pez.github.io/Portfolio/img/pcbback1.jpg' alt="pcbimg" />
+                    <img className='profileimg' src='https://alf0ns0-l0pez.github.io/Portfolio/img/perfil_gray_fix1.png' alt="profileimage" />
+                </section>
+            </header>
+            <section id="opensource">
+                <h1 >Experience</h1>
+                <div className="linux-message">
+                    <img className="linuxtorvalds-image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/LinuxCon_Europe_Linus_Torvalds_03_%28cropped%29.jpg/220px-LinuxCon_Europe_Linus_Torvalds_03_%28cropped%29.jpg" alt="linuxtorvalds" />
+                    <p>I often compare open source to science. Science took
+                        this whole notion of developing ideas in the open and
+                        improving on other peoples' ideas. It made science what
+                        it is today and made the incredible advances that we have
+                        had possible. And I compare that to witchcraft and alchemy,
+                        where openness was something you didn't do.</p>
+                </div>
+            </section>
+
+        </>
     );
 };
--- src/home/TopMenu.js
+++ src/home/TopMenu.js
@@ -2,40 +2,17 @@ import React from 'react';
 import '../index.css';
 
 const pages = ['Home', 'Platforms', 'Experiences', 'Contact'];
-export default function TopMenu(props) {
-    const { isMobile, dataRef } = props;
-    const handleCloseMenu = (event,select) => {
-        event.preventDefault();
-        if(select='Home'){
-            dataRef[0].current.scrollIntoView({ behavior: 'smooth' });
-        }
-        if(select='Platforms'){
-            dataRef[1].current.scrollIntoView({ behavior: 'smooth' });
-        }
-        if(select='Experiences'){
-            dataRef[2].current.scrollIntoView({ behavior: 'smooth' });
-        }
-        if(select='Contact'){
-            dataRef[3].current.scrollIntoView({ behavior: 'smooth' });
-        }
-    }
+export default function TopMenu() {
     return (
-        <div className='top_menu' style={isMobile ? {height:'18rem'}:{height:'10rem'}}>
+
+            {/*
+            <div className='top_menu' style={isMobile ? {height:'18rem'}:{height:'10rem'}}>
             <img 
                 src='https://alf0ns0-l0pez.github.io/Portfolio/img/al_logo.png' 
                 className={isMobile ? 'logo_menu_mobile':'logo_menu'} 
                 alt="..."/>
-            
+            </div>
+            */}
 
-            {/*!isMobile?<form style={{marginRight:'4rem'}}>{pages.map((page) => (
-                <button
-                    className={page=='Home'?'button_menu_home':'button_menu'}
-                    key={page}
-                    onClick={() =>handleCloseMenu(page)}
-                >
-                    <b>{page}</b>
-                </button>
-            ))}</form>:<div/>*/}
-        </div>
     );
 };
