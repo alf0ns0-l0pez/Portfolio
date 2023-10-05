d54de41433ebf3b6c43ed868981a29c95945e52a
unknown <alfonsoloph@gmail.com>
2023-10-04 19:44:35 -0600

    change header and profile

--- src/home/App.js
+++ src/home/App.js
@@ -7,8 +7,10 @@ export const App = () => {
                 <img src='https://alf0ns0-l0pez.github.io/Portfolio/img/al_logo.png'
                     alt="alfonsologo"  />
                 <section>
-                    <img className='pcbimg' src='https://alf0ns0-l0pez.github.io/Portfolio/img/pcbback1.jpg' alt="pcbimg" />
-                    <img className='profileimg' src='https://alf0ns0-l0pez.github.io/Portfolio/img/perfil_gray_fix1.png' alt="profileimage" />
+                    <img className='pcbimg' src='https://alf0ns0-l0pez.github.io/Portfolio/img/pcbbackground.jpg' alt="pcbimg" />
+                    <img className='profileimg' src='https://alf0ns0-l0pez.github.io/Portfolio/img/profile.png' alt="profileimage" />
+                    {/*<img className='pcbimg' src='https://alf0ns0-l0pez.github.io/Portfolio/img/pcbback1.jpg' alt="pcbimg" />
+                    <img className='profileimg' src='https://alf0ns0-l0pez.github.io/Portfolio/img/pcbbackground.png' alt="profileimage" />*/}
                 </section>
             </header>
             <section id="opensource">
